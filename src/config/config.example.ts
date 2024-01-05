ff3b2a84a0cbebb6719312884722e62e6f9d00e1
yusufkaraasln <yusufkaraasln@gmail.com>
2024-01-05 08:45:58 +0300

    Request helper class

--- src/config/config.example.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const apiUrl = 'http://127.0.0.1:5000'
-
-export const axiosInstance = axios.create({
-    baseURL: apiUrl,
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
